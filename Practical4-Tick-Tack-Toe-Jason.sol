04badebf977580fc9b8ba36ddca5536cd7bdf769
Jason Chen <jasonchen990513@gmail.com>
2024-01-18 18:10:26 +0800

    update practical 4 complete all function and add comment

--- Practical4-Tick-Tack-Toe-Jason.sol
+++ Practical4-Tick-Tack-Toe-Jason.sol
@@ -5,14 +5,14 @@ contract TickTackToe{
     //validate the user
     address public player1address; //store user 1 address
     address public player2address; //store uesr 2 address
-    uint public playerCount = 0;
+    uint public playerCount = 0; //count current user
 
     //User 1 set to X    User 2 set to O     
     // basic game logic
-    string[] public options = ["", "", "", "", "", "", "", "", ""];
-    bool public activate = false;
-    string public currentPlayer = "X";
-    uint[][] private winConditionSet= 
+    string[] public options = ["", "", "", "", "", "", "", "", ""]; //option to store player option
+    bool public activate = false; // initial game status is false
+    string public currentPlayer = "X"; // frist player is X
+    uint[][] private winConditionSet=  
     [[0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -20,21 +20,27 @@ contract TickTackToe{
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]];
+    [2, 4, 6]]; //set all win condition
+                // tick tack toe grid
+                // 0 1 2
+                // 3 4 5
+                // 6 7 8
     
-    event showResult (string result);
-    event queueFull(string message);
-    event metchSuccessful(string message);
-    event startGame(string message, address player1, address player2);
-    event nextUser(address nextUser);
-    event updateUI(string[] options);
+    event showResult (string result); // send the game result to UI
+    event startGame(string message, address player1, address player2); // send the player in this metch
+    event nextUser(address nextUser); // send next user address
+    event updateUI(string[] options); // send message to update UI
+    event nextUesrStr(string next); // send next uesr (X or O)
+    event resetUI(string initPLayer); // use to trigger front end reset function   
 
+    // for front to get option array
     function getoption () external view returns (string[] memory){
         return options;
     }
 
     // user queue
     function joinGame () external {
+        //allow player to join if player not more than 2 
         if(playerCount == 0){
             restartGame();
             player1address = msg.sender;
@@ -47,32 +53,43 @@ contract TickTackToe{
             //send message to inform the player start game
             emit startGame("Start game!", player1address, player2address);
         }else{
-            emit queueFull("Now queue is full, try later");
+            //if player more than 2, send the message to front end
+            require(playerCount < 2,"Now queue is full, try later");
         }
     } 
 
     // basic game logic
+    // check player input and ensure the input is from correct player 
     function userInput(uint input) external {
-        require(activate,"The game still not start yet");
-        require(compareStrings(options[input],""),"Invalid input");
+        require(activate,"The game still not start yet");           //check game status
+        require(compareStrings(options[input],""),"Invalid input"); //check the input
+        //check player
         if(compareStrings(currentPlayer, "X")){
             require(msg.sender == player1address,"You are not player 1");
         }else{
             require(msg.sender == player2address,"You are not player 2");
         }
-        options[input] = currentPlayer;
-        emit updateUI(options);
-        checkWinner();
+        
+        options[input] = currentPlayer; //if both no problem, just put into options
+        emit updateUI(options);         //sned data for UI display
+        checkWinner();                  //check the win condition
     }
     // use to check the win, draw  
     function checkWinner () private {
         bool userWon = false;
+        //get the win condition on array and do the condition check
         for(uint i = 0 ; i < winConditionSet.length; i++){
             uint[] memory winCondition = winConditionSet[i];
+            //get the win condition and check the element
             string memory resultA= options[winCondition[0]];
             string memory resultB= options[winCondition[1]];
             string memory resultC= options[winCondition[2]];
-
+            // tick tack toe grid
+            // 0 1 2
+            // 3 4 5
+            // 6 7 8
+            //compare the option on the array
+            //if all are same, then that player win
             if(compareStrings(resultA, "")||compareStrings(resultB, "")||compareStrings(resultC, "")){
                 continue;
             }
@@ -81,33 +98,43 @@ contract TickTackToe{
                 break;
             }
         }
-
+        //if someone win or draw, send message to front end
+        //else change player and go to next round
         if(userWon){
-            //sent message
+            //sent winer to info UI
             emit showResult(currentPlayer);
+            //reset attribute
             activate = false;
             playerCount = 0;
         } else if(checkOptions()){
+            // send Draw message to UI
             emit showResult("Draw");
+            //reset attribute
             activate = false;
             playerCount = 0;
         } else {
+            //change to next player
             changePlayer();
         }
     }
-    event nextUesrStr(string next);
+    //change player per round
     function changePlayer() private {
+        //send the change message to front end
         if(compareStrings(currentPlayer, "X")){
             emit nextUser(player2address);
         }else{
             emit nextUser(player1address);
         }
-        //change the player per round
+        //change the player
         currentPlayer = (compareStrings(currentPlayer, "X")) ? "O" : "X";
+        //sent the player info to UI
         emit nextUesrStr(currentPlayer);
     }
 
+    // Use to check if there are any empty options.
     function checkOptions() private view returns (bool) {
+        //check each element in options array
+        //if include any "" return false, else return true
         for(uint i = 0 ; i < options.length; i++){
             if(compareStrings(options[i], "")){
                 return false;
@@ -116,19 +143,19 @@ contract TickTackToe{
         return true;
     }
 
-
     //using hash function to compare the string.
     function compareStrings(string memory a, string memory b) public pure returns (bool) {
         //Use bytes32 to store the result from keccak256 hash function
         return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
     }
 
-    event resetUI(string initPLayer);
+    //use to reset game information
     function restartGame() public {
         currentPlayer = "X";
         options = ["", "", "", "", "", "", "", "", ""];
         emit resetUI(currentPlayer);
         playerCount = 0;
+        activate = false;
         player1address = address(0);
         player2address = address(0);
     }
