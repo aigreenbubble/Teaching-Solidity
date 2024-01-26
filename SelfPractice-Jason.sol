84e51edc03d25e08d3cb0179d78093a0e744b327
Jason Chen <jasonchen990513@gmail.com>
2024-01-26 15:29:30 +0800

    update

--- SelfPractice-Jason.sol
+++ SelfPractice-Jason.sol
@@ -1,23 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+//import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract payAndPlay{
-    uint private salt = 0; 
-    uint public numberCount = 1;
-    address payable public contractAddress = payable(address(this));
-    address playerAddress;
-    address public owner;
-    bool locked;
+    uint private salt = 0; //salt for random number generate
+    uint public numberCount = 1; // count now many user
+    address payable public contractAddress = payable(address(this)); // declare contract address
+    address playerAddress; // recoed player address
+    address public owner; // contract owner
+    bool locked;    // lock for re entry
 
     event showBalance(uint);
     event showResult(uint);
     event showError(string);
 
+    //check the message sender is owner or not
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
         _;
     }
-
+    //avoid re entry 
     modifier noReentrancy() {
         require(!locked, "Error: Reentrant call");
         locked = true;
@@ -28,47 +33,52 @@ contract payAndPlay{
     constructor() {
         owner = msg.sender;
     }
-
+    //get current player balance
     function getPlayerBalance() external view returns(uint) {
         return msg.sender.balance;
     }
-
-    function getBalance() external view returns(uint) {
+    //get the contract balance
+    function getBalance() external view  returns(uint) {
         return contractAddress.balance;
     }
 
     function depositeMoney () public payable {
-        contractAddress.transfer(msg.value);
-        //bool success = contractAddress.send(msg.value);
+        //contractAddress.transfer(msg.value);
+        //bool success = payable(contractAddress).send(msg.value);
+        //require(success, "something wrong");
         // if(!success){
         //     emit showError("something wrong");
         // }
         emit showBalance(contractAddress.balance);
 
     }
-
-    function withdrawMoney () public payable {
-        require(msg.sender == playerAddress,"You are not winer");
-        payable(msg.sender).transfer(contractAddress.balance);
-        emit showBalance(contractAddress.balance);
+    //let winer to get their reward
+    function withdrawMoney () public {
+        require(msg.sender == playerAddress,"You are not winer"); //check player who what to withdraw money is winer or not
+        payable(msg.sender).transfer(contractAddress.balance); // transfer reward to winer address
+        emit showBalance(contractAddress.balance); // show current balance
 
     }
-
-    function getAllMoney() external payable onlyOwner{
+    // fall back all token to owner wallet address
+    // only own can execute this function
+    function getAllMoney() external onlyOwner{
         payable(msg.sender).transfer(contractAddress.balance);
     }
 
-    receive() external payable{
-        contractAddress.send(msg.value);
-     }
 
     //game logic
     function playGame(uint guessNumber)external payable noReentrancy{
-        require(msg.value >= 1 ether,"send 1 ether!");
-        numberCount++;
+        require(msg.value >= 1 ether,"send 1 ether!"); // require player atleast put 1 token to play
+        //increase the number count when it not more than 10
+        if(numberCount <= 10){
+            numberCount++;
+        }
+        //win condition check
         if(getRandomNumber() == guessNumber){
+            //winer get the reward
             playerAddress = msg.sender;
             withdrawMoney();
+            //initail the game information 
             playerAddress = address(0);
             numberCount = 2;
         } else{
